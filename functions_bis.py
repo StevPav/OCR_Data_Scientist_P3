bc3787ac64d6482e9d93873ec118a545cceab013
StevPav <stevanpaven@gmail.com>
2021-09-01 14:47:23 +0200

    Ajout des fichiers de travail

---
+++ functions_bis.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+from scipy.cluster.hierarchy import dendrogram
+
+def plot_dendrogram(Z, names):
+    plt.figure(figsize=(10,25))
+    plt.title('Hierarchical Clustering Dendrogram')
+    plt.xlabel('distance')
+    dendrogram(
+        Z,
+        labels = names,
+        orientation = "left",
+    )
+    plt.show()
